60475ce9ae2d160641404f9fb0cbb016a763a438
Sergio Alfaro <39069474+wesbrek@users.noreply.github.com>
2021-03-03 19:16:01 -0600

    Basic project

---
+++ mern-task-client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Projects from './components/projects/Projects';
+import NewUser from './components/auth/NewUser';
+import Login from './components/auth/Login';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/new-user" component={NewUser} />
+        <Route exact path="/projects" component={Projects} />
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
